d8ba2548d951332547f178127c57ef03e021ae0a
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-22 01:31:06 +0200

    [add] add img, name and role

--- script.js
+++ script.js
@@ -3,39 +3,40 @@ const row = document.querySelector('.row');
 // Definisco un array di oggetti con il nome "users", dentro ci metto diverse proprietà, tra cui nome, ruolo, foto e aggettivi.
 const users = [
     {
-        nome: '',
-        ruolo: '',
-        foto: '',
+        nome: 'Wayne Barnet',
+        ruolo: 'Founder & CEO',
+        foto: 'img/wayne-barnett-founder-ceo.jpg',
     },
 
     {
-        nome: '',
-        ruolo: '',
-        foto: '',
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'img/angela-caroll-chief-editor.jpg',
     },
 
     {
-        nome: '',
-        ruolo: '',
-        foto: '',
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: 'img/angela-lopez-social-media-manager.jpg',
     },
 
     {
-        nome: '',
-        ruolo: '',
-        foto: '',
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto: 'img/walter-gordon-office-manager.jpg',
     },
 
     {
-        nome: '',
-        ruolo: '',
-        foto: '',
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto: 'img/scott-estrada-developer.jpg',
     },
 
     {
-        nome: '',
-        ruolo: '',
-        foto: '',
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: 'img/barbara-ramos-graphic-designer.jpg',
     },
 
-]
+];
+console.log(users)
