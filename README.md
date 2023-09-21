f276e015593b149d404e8f5cd1cfa22293aecdd1
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-22 01:24:55 +0200

    [add] metto un arrey di oggetti (nome, ruolo, ecc)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Our Team
 - Creo la struttura con HTML e CSS
 - Definisco un elemento HTML con classe "row" tramite la variabile row. Questo sarà il contenitore delle card degli utenti.
+- Definisco un array di oggetti con il nome "users", dentro ci metto diverse proprietà, tra cui nome, ruolo e foto.
--- script.js
+++ script.js
@@ -1,2 +1,41 @@
 // Definisco un elemento HTML con classe "row" tramite la variabile row. Questo sarà il contenitore delle card degli utenti.
 const row = document.querySelector('.row');
+// Definisco un array di oggetti con il nome "users", dentro ci metto diverse proprietà, tra cui nome, ruolo, foto e aggettivi.
+const users = [
+    {
+        nome: '',
+        ruolo: '',
+        foto: '',
+    },
+
+    {
+        nome: '',
+        ruolo: '',
+        foto: '',
+    },
+
+    {
+        nome: '',
+        ruolo: '',
+        foto: '',
+    },
+
+    {
+        nome: '',
+        ruolo: '',
+        foto: '',
+    },
+
+    {
+        nome: '',
+        ruolo: '',
+        foto: '',
+    },
+
+    {
+        nome: '',
+        ruolo: '',
+        foto: '',
+    },
+
+]
